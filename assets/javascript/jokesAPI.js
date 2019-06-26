55b3e6dbd9569021897752f73de92b756898044d
wkpc <wkeel95@gmail.com>
2019-06-26 15:45:12 -0400

    init commit

--- assets/javascript/jokesAPI.js
+++ assets/javascript/jokesAPI.js
@@ -1,4 +1,4 @@
-      var search = 
+      var search = "";
       var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=WLBFiQ7GnS37bP29n3qxnRk7MOIciO94&q=" + search + "&limit=10&offset=0&rating=G&lang=en";
   $.ajax({
       url: queryURL,
