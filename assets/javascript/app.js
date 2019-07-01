47ea88ff8b5f08c04349aa677bf659884957bae3
Deli <turtle.7.hennessy@gmail.com>
2019-07-01 11:29:37 -0400

    updating app.js with on hover effects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,17 @@
 $("#block1").hover(function(){
-    $("#block2").hide();
-    $("#block3").hide();
+    $("#response1").show();
+    $("#response2").hide();
+    $("#response3").hide();
+})
+
+$("#block2").hover(function(){
+    $("#response2").show();
+    $("#response1").hide();
+    $("#response3").hide();
+})
+
+$("#block3").hover(function(){
+    $("#response3").show();
+    $("#response1").hide();
+    $("#response2").hide();
 })
