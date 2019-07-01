f9b8e53e242f13708eeca3dd9e704d16df42ce42
Deli <turtle.7.hennessy@gmail.com>
2019-07-01 13:44:41 -0400

    renamed index2 to index and deleted index2. updated TILReddiAPI.js with user validation and search bar. Also removed button from reddit api and added search bar.

--- assets/javascript/TILRedditAPI.js
+++ assets/javascript/TILRedditAPI.js
@@ -1,17 +1,26 @@
-$("#block2").on("click", function(event) {
-  console.log("facts!!!")
-  event.preventDefault();
-  $("#response2").empty();
-  var queryURL = "https://www.reddit.com/r/todayilearned/search.json?q=glow&limit=25&sort=top&restrict_sr=1";
+function til() {
+  var search = $("#search").val().trim();
+  var queryURL = "https://www.reddit.com/r/todayilearned/search.json?q=" + search + "&limit=25&sort=top&restrict_sr=1";
 
-$.ajax({
+  $.ajax({
     url: queryURL,
     method: "GET",
-}).then(function (response) {
+  }).then(function (response) {
+    console.log(response);
+    if (response.data.after === null) {
+      var h1 = $("<p>").text("Either you didnt put anything in the search box, there aren't results for what ou searched. Try a synonym of what you were searching for or actually type a word :)");
+      $("#response2").append(h1);
+    }
+    var h1 = $("<p>").text("You hear about this? Have you heard about this: " + response.data.children[Math.floor(Math.random() * 25)].data.title);
+    console.log(response.data.children[Math.floor(Math.random() * 25)].data.title);
+    $("#response2").append(h1);
+  })
+}
 
-      var h1 = $("<p>").text("You hear about this? Have you heard about this: " + response.data.children[Math.floor(Math.random()*25)].data.title);
-        console.log(response.data.children[Math.floor(Math.random()*25)].data.title);
-        $("#response2").append(h1);
 
-    })
+  $("#block2").on("click", function (event) {
+    console.log("facts!!!")
+    event.preventDefault();
+    $("#response2").empty();
+    til();
   })
