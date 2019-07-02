2595d8d0af26607f3cb928934e2709c336f97770
shintakuix <candeiasva@gmail.com>
2019-07-02 13:13:59 -0400

    added a timeup to prevent from doing more than 1 click per second

--- assets/javascript/nytAPI.js
+++ assets/javascript/nytAPI.js
@@ -1,29 +1,36 @@
 $("#block3").on("click", function(event) {
-  console.log("works!!!");
+/*   console.log("works!!!"); */
   event.preventDefault();
   /* console.log("works"); */
+
+  var delayButton =  $(this);
+    delayButton.attr('disabled', true);
+    setTimeout(function() {
+        delayButton.attr('disabled', false);
+    }, 1000);
   $("#response3").empty();
   
   var currentTime = moment();
   var todayFormat = moment(currentTime).format("YYYYMMDD")
-  console.log(todayFormat);
+/*   console.log(todayFormat); */
 
   var pastTime = moment().subtract("2", "months");
   var fewMonths = moment(pastTime).format("YYYYMMDD");
-  console.log(fewMonths);
+/*   console.log(fewMonths); */
 
 
   var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=sports&facet=true&begin_date=" + fewMonths + "&end_date=" + todayFormat +  "&api-key=e8Sb2nfsg50GLoGeAhKQFkDMQzyjIhkE";
-  console.log(queryURL);
+  
+ /*  console.log(queryURL); */
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) { 
-    console.log(response);
+/*     console.log(response); */
 
     var randomArticle = response.response.docs[Math.floor(Math.random() * 10)];
-    console.log(randomArticle);
+/*     console.log(randomArticle); */
   
     var h1 = $("<p>").text(randomArticle.headline.main);
     var headBold = $("<b>").text("Headline: ");
